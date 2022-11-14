332dd53330e41e3ad71ca6b741168eaf1f9b1682
nuray-alam <siddik4x@gmail.com>
2022-11-14 12:29:57 +0600

    get services with available slots api added

--- index.js
+++ index.js
@@ -19,8 +19,11 @@ async function run() {
 
     try {
         await client.connect();
+        // all collections
         const serviceCollection = client.db('doctors_portal_practice').collection('services');
+        const bookingCollection = client.db('doctors_portal_practice').collection('bookings');
 
+        //get all available appointments / services api
         app.get('/service', async (req, res) => {
 
             const query = {};
@@ -29,6 +32,55 @@ async function run() {
             res.send(services);
 
         })
+
+        //add booking api
+        app.post('/booking', async (req, res) => {
+
+            const booking = req.body;
+            const query = { treatment: booking.treatment, date: booking.date, patient: booking.patient }
+            const exists = await bookingCollection.findOne(query);
+            if (exists) {
+                return res.send({ success: false, booking: exists })
+            }
+            const result = await bookingCollection.insertOne(booking);
+            return res.send({ success: true, result });
+
+        })
+
+
+        app.get('/available', async (req, res) => {
+
+            const date = req.query.date || 'Nov 14, 2022';
+
+            //Step1: get all services
+            const services = await serviceCollection.find().toArray();
+
+            //Step2: get the booking of that day
+            const query = { date: date };
+            const bookings = await bookingCollection.find(query).toArray();
+
+            //Step3: for each service, find bookings for the service
+            services.forEach(service => {
+
+                const serviceBookings = bookings.filter(b => b.treatment === service.name);
+                // const booked = serviceBookings.map(s => s.slot);
+                service.booked = serviceBookings.map(s => s.slot);
+                service.available = service.slots.filter(x => !service.booked.includes(x))
+            })
+
+
+            res.send(services)
+
+        })
+
+        /**
+         * API Naming Convention
+         * app.get('/booking') // get all bookings in this collection or get more than one or by filter
+         * app.get('/booking/:id') // get a specific booking
+         * app.post('/booking') // add a new booking
+         * app.patch('/booking/:id') // update a specific booking
+         * app.delete('/booking/:id') // delete a specific booking
+         */
     }
     finally {
 
